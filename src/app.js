d27c56f85c883da1904f6d07c410d0a4f2e0881d
KumarBadal208 <kumarbadal208@gmail.com>
2024-11-28 22:29:04 +0530

    error handler

--- src/app.js
+++ src/app.js
@@ -12,9 +12,20 @@ app.get('/user',auth, (req, res, next)=>{
 }
 );
 
-app.use('/',(req,res)=>{
-    res.send('hello there  !!');
+app.get('/getUserData',(req,res)=>{
+    throw new Error("hfadsjk");
+    res.send("user data sent");
 })
+
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        res.status(500).send('internal server error');
+    }
+});
+
+// app.use('/',(req,res)=>{
+//     res.send('hello there  !!');
+// })
 
 app.listen(3000, ()=>{
     console.log("server is running on 3000");
